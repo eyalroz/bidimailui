e66b299c2d2c17f9583ece3941929d984a8af186
Eyal Rozenberg <eyalroz@technion.ac.il>
2004-08-24 22:56:32 +0000

    The thunderbird extension

---
+++ source/tbird/chrome/content/bidimailpack/bidimailpack-prefs.js
@@ -0,0 +1,40 @@
+function initValues()
+{
+  var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+  var element;
+
+  try {
+    element = document.getElementById('bidimailpack-autodetect');
+    element.checked = prefs.getBoolPref('mailnews.message_display.autodetect_direction');
+  } catch(e) {};
+  try {
+    element = document.getElementById('bidimailpack-default-dir');
+    prefDir = prefs.getCharPref('mailnews.send_default_direction');
+    if ( (prefDir == 'ltr') || (prefDir == 'LTR') )
+      element.selectedItem = element.childNodes[0];
+    else element.selectedItem = element.childNodes[1];
+  } catch(e) {};
+  try {
+    element = document.getElementById('bidimailpack-reply-in-default-dir');
+    element.checked = prefs.getBoolPref('mailnews.reply_in_default_direction');
+  } catch(e) {};
+  try {
+    element = document.getElementById('bidimailpack-display-buttons');
+    element.checked = prefs.getBoolPref('mail.compose.show_direction_buttons');
+  } catch(e) {};
+}
+
+function saveValues()
+{
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+
+    element = document.getElementById('bidimailpack-autodetect');
+    prefs.setBoolPref('mailnews.message_display.autodetect_direction', element.checked);
+    element = document.getElementById('bidimailpack-default-dir');
+    prefs.setCharPref('mailnews.send_default_direction', element.value);
+    element = document.getElementById('bidimailpack-reply-in-default-dir');
+    prefs.setBoolPref('mailnews.reply_in_default_direction', element.checked);
+    element = document.getElementById('bidimailpack-display-buttons');
+    prefs.setBoolPref('mail.compose.show_direction_buttons', element.checked);
+
+}
