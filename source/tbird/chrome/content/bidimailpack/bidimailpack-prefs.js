7a0a1dd7e38d31b0e15f02beb457e073f4e7e339
Eyal Rozenberg <eyalroz@technion.ac.il>
2004-09-07 23:01:17 +0000

    propagated RLM/LRM support to suite; in suite prefs, direction LTR is now listed before RTL; some comment & en-US locale tweaks

--- source/tbird/chrome/content/bidimailpack/bidimailpack-prefs.js
+++ source/tbird/chrome/content/bidimailpack/bidimailpack-prefs.js
@@ -3,13 +3,13 @@ function initValues()
   var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
   var element;
 
-  // Auto detect text-plain incomming messages direction. default: true.
+  // Auto detect text-plain incomming messages direction. Default: true.
   try {
     element = document.getElementById('bidimailpack-autodetect');
     element.checked = prefs.getBoolPref('mailnews.message_display.autodetect_direction');
   } catch(e) {};
-  
-  // Default composing direction "hook". default: none.
+
+  // Default composing direction for a new message. Default: ltr.
   try {
     element = document.getElementById('bidimailpack-default-dir');
     prefDir = prefs.getCharPref('mailnews.send_default_direction');
@@ -19,13 +19,16 @@ function initValues()
       element.selectedItem = element.childNodes[1];
   } catch(e) {};
 
-  // Reply direction options: (1) same direction as orginal (2) force default direction. Default: (1)
+  // Reply direction options: 
+  //   - same direction as the orginal message, or
+  //   - force default direction
+  // Default: same direction as the original message
   try {
     element = document.getElementById('bidimailpack-reply-in-default-dir');
     element.checked = prefs.getBoolPref('mailnews.reply_in_default_direction');
   } catch(e) {};
 
-  // show direction button? default: true.
+  // Show direction control button when composing a message? Default: True
   try {
     element = document.getElementById('bidimailpack-display-buttons');
     element.checked = prefs.getBoolPref('mail.compose.show_direction_buttons');
@@ -33,7 +36,7 @@ function initValues()
     element.checked = true;
   };
 
-  // rlm/lrm contextual menu items. default: false
+  // RLM/LRM contextual menu item visibile? Default: False
   try {
     element = document.getElementById('bidimailpack-display-context-control-charcters');
     element.checked = prefs.getBoolPref('mail.compose.show_context_control_characters');
