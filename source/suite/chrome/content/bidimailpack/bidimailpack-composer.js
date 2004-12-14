af34dfdc6cea9b9e8c77039de4decc44de396159
Eyal Rozenberg <eyalroz@technion.ac.il>
2004-12-14 02:11:00 +0000

    [suite] use margin prefs for first paragraph

--- source/suite/chrome/content/bidimailpack/bidimailpack-composer.js
+++ source/suite/chrome/content/bidimailpack/bidimailpack-composer.js
@@ -344,6 +344,14 @@ function loadParagraphMode() {
       // paragraph are block elements, with a direction setting
       try {
         doStatefulCommand('cmd_paragraphState', "p");
+        var editor = GetCurrentEditor();
+        if (editor) {
+          doStatefulCommand('cmd_paragraphState', "p");
+          var par = findClosestBlockElement(editor.selection.focusNode);
+          // Set Paragraph Margins
+          par.style.marginTop    = gParagraphMarginTop;
+          par.style.marginBottom = gParagraphMarginBottom;
+        }
       } catch(e) {
         // since the window is not 'ready', something might throw
         // an exception here, like inability to focus etc.
