807a86a88a6b5311d2758b709160071d5d5381e0
Eyal Rozenberg <eyalroz@technion.ac.il>
2005-12-02 15:43:44 +0000

    fix for bug 7285 - now supporting suite composer fixed a = instead of == bug in bidimailpack-composer.js spacing and indentation issues restored overwritten arabic localization in suite

---
+++ source/suite/chrome/content/bidimailpack/bidimailpack-editor.js
@@ -0,0 +1,56 @@
+function EditorWindowOnLoad() {
+  gLoadEventCount += 1;
+  if (gLoadEventCount != 3) return; // aparently 3rd time's the charm for the editor
+  gLastWindowToHaveFocus = null;
+
+  top.controllers.insertControllerAt(1, directionSwitchController);
+
+  HandleDirectionButtons();
+
+  var documentParams = {
+    isEmpty: false
+  };
+
+  DetermineNewDocumentParams(documentParams);
+  SetInitialDocumentDirection(documentParams);
+   
+  gAlternativeEnterBehavior =
+    gBDMPrefs.getBoolPref("compose.alternative_enter_behavior", true);
+  if (gAlternativeEnterBehavior)
+    LoadParagraphMode();
+
+  directionSwitchController.setAllCasters();
+}  
+
+
+
+function InstallEditorWindowEventHandlers() {
+  document.addEventListener("load", EditorWindowOnLoad, true);
+  document.addEventListener("keypress", onKeyPress, true);
+}
+
+function DetermineNewDocumentParams(messageParams) {
+  var body = document.getElementById("content-frame").contentDocument.body;
+
+  try {
+    if (!body.hasChildNodes()) 
+      messageParams.isEmpty = true;
+    else if ( body.hasChildNodes() &&
+             !body.firstChild.hasChildNodes() ) {
+      if ((body.firstChild == body.lastChild) &&
+          (body.firstChild.nodeName == "BR"))
+        messageParams.isEmpty = true;
+    }
+    else {
+      if (body.firstChild == body.lastChild &&
+          body.firstChild.nodeName == "P" &&
+          body.firstChild.firstChild.nodeName == "BR" &&
+          body.firstChild.firstChild == body.firstChild.lastChild)
+        messageParams.isEmpty = true;
+    }
+  }
+  catch(e) {
+    // can't get elements - must be empty...
+    messageParams.isEmpty = true;
+  }
+}
