3810282d16c28eb76a791f8cdc62fb83de131072
Eyal Rozenberg <eyalroz@technion.ac.il>
2008-08-08 07:23:31 +0000

    additional checkins for bug 13028 - last-used direction for composition

--- source/tbird/defaults/preferences/bidimailui.js
+++ source/tbird/defaults/preferences/bidimailui.js
@@ -10,3 +10,4 @@ pref("bidiui.mail.display.autodetect_bidi_misdecoding", true);
 pref("bidiui.mail.compose.alternative_enter_behavior", true);
 pref("bidiui.mail.display.user_accepts_unusable_charset_pref", false);
 pref("bidiui.mail.display.decode_numeric_html_entities", true);
+pref("bidiui.mail.compose.last_used_direction", "ltr");
