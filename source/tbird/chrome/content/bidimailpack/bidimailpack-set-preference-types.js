d2f8473134646aafbd511b72a12b0d02d6ccad17
Eyal Rozenberg <eyalroz@technion.ac.il>
2019-09-28 22:17:39 +0000

    Fixes bug #26709:
    * Preference type for `show_direction_buttons` now correctly set to `"bool"`.

--- source/tbird/chrome/content/bidimailpack/bidimailpack-set-preference-types.js
+++ source/tbird/chrome/content/bidimailpack/bidimailpack-set-preference-types.js
@@ -4,7 +4,7 @@
 Preferences.addAll([
   { id: "extensions.bidiui.mail.compose.default_direction",                   type: "string" },
   { id: "extensions.bidiui.mail.compose.reply_in_default_direction",          type: "bool"   },
-  { id: "extensions.bidiui.mail.compose.show_direction_buttons",              type: "string" },
+  { id: "extensions.bidiui.mail.compose.show_direction_buttons",              type: "bool"   },
   { id: "extensions.bidiui.mail.compose.start_composition_in_paragraph_mode", type: "bool"   },
   { id: "extensions.bidiui.mail.compose.space_between_paragraphs.value",      type: "string" },
   { id: "extensions.bidiui.mail.compose.space_between_paragraphs.scale",      type: "string" },
