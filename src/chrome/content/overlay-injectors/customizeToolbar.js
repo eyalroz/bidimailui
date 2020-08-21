65c8df96ae46e6f51a4bd5800a70f756c456fe89
Eyal Rozenberg <eyalroz@technion.ac.il>
2020-08-21 13:15:18 +0300

    Now setting the platform attribute on the root element of each document we're styling (except for messages proper).

--- src/chrome/content/overlay-injectors/customizeToolbar.js
+++ src/chrome/content/overlay-injectors/customizeToolbar.js
@@ -5,6 +5,16 @@ console.log("top level code of customizeToolbar overlay injector script!");
 function onLoad(activatedWhileWindowOpen) {
   console.log("in customizeToolbar overlay injector script - onLoad!");
   WL.injectCSS("chrome://bidimailui/content/skin/classic/bidimailui.css");
+
+  // Since we no longer have per-platform-skin support, we set this attribute
+  // on our root element, so that, in our stylesheet, we can contextualize using
+  // this attribute, e.g.
+  //
+  // [platform="Darwin"] someElement {
+  //     background-color: red;
+  // }
+  //
+  document.documentElement.setAttribute("platform", Services.appinfo.os);
 }
 
 // called on window unload or on add-on deactivation while window is still open
--- src/chrome/content/overlay-injectors/messengercompose.js
+++ src/chrome/content/overlay-injectors/messengercompose.js
@@ -197,6 +197,15 @@ function onLoad(activatedWhileWindowOpen) {
       document.addEventListener("keyup",   BiDiMailUI.Composition.onKeyUp,   capture);
     }
 
+  // Since we no longer have per-platform-skin support, we set this attribute
+  // on our root element, so that, in our stylesheet, we can contextualize using
+  // this attribute, e.g.
+  //
+  // [platform="Darwin"] someElement {
+  //     background-color: red;
+  // }
+  //
+  document.documentElement.setAttribute("platform", Services.appinfo.os);
 }
 
 // called on window unload or on add-on deactivation while window is still open
